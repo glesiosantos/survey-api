85d65f91cc5b978ee99edde0943e5d25c0d0a37f
Glêsio Santos <glesioss@gmail.com>
2023-07-31 20:21:30 -0300

    test: ensure BcrypterAdapter return a hash on success

--- src/infra/criptography/encrypter.spec.ts
+++ src/infra/criptography/encrypter.spec.ts
@@ -1,12 +1,25 @@
 import bcrypt from 'bcrypt'
 import { BCrypterAdapter } from './bcrypter_adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => { resolve('hashed_password') })
+  }
+}))
+
 describe('BCrypt Adapter', () => {
-  it('should call bcrypter with correct value', async () => {
+  it('should call bcrypter with correct values', async () => {
     const salt = 12
     const sut = new BCrypterAdapter(salt)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  it('should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BCrypterAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hashed_password')
   })
 })
