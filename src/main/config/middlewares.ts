58c97f4ba40011b0f5836195fd52800dbafddae2
Glêsio Santos <glesioss@gmail.com>
2023-08-16 16:08:28 -0300

    test: ensure content types return type as json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlewares/body_parser'
 import { cors } from '../middlewares/cors'
+import { contentTypes } from '../middlewares/content_type'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentTypes)
 }
