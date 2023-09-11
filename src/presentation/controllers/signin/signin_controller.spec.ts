0e407eecf75c92de5fd17de42091bc763df429a0
Glêsio Santos <glesioss@gmail.com>
2023-09-11 17:12:14 -0300

    test: ensure SignInController return a badRequest when invalid email is provided

--- src/presentation/controllers/signin/signin_controller.spec.ts
+++ src/presentation/controllers/signin/signin_controller.spec.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http_helper'
 import { Controller, HttpRequest } from '../../protocols'
 import { EmailValidator } from '../signup/signup_protocols'
@@ -61,5 +61,12 @@ describe('Sign In Controller', () => {
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
     await sut.handle(makeFakeRequest())
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
+  it('should return badRequest when invalid e-mail provided', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValue(false)
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
   })
 })
