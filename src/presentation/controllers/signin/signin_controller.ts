ca4f3ce4b6795ffdde5e2b5428c990aab566a888
Glêsio Santos <glesioss@gmail.com>
2023-09-11 16:52:08 -0300

    feat: ensure SignInController return a bad request when password is no provided

--- src/presentation/controllers/signin/signin_controller.ts
+++ src/presentation/controllers/signin/signin_controller.ts
@@ -3,6 +3,11 @@ import { MissingParamError, badRequest } from '../signup/signup_protocols'
 
 export class SignInController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return badRequest(new MissingParamError('email'))
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError('email'))
+    }
+    if (!httpRequest.body.password) {
+      return badRequest(new MissingParamError('password'))
+    }
   }
 }
