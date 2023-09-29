324de8a59183b469adbd7ab7a67b3dbdc7b94070
Glêsio Santos <glesioss@gmail.com>
2023-09-29 05:06:46 -0300

    test: add signin route

--- src/main/routers/auth_router.test.ts
+++ src/main/routers/auth_router.test.ts
@@ -1,20 +1,27 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/mongo_helper'
+import { Collection } from 'mongodb'
+import bcrypt from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Auth Router', () => {
   beforeAll(async () => { await MongoHelper.connect(process.env.MONGO_URL) })
 
   afterAll(async () => { await MongoHelper.disconnect() })
 
-  beforeEach(async () => await MongoHelper.getCollection('accounts').deleteMany({}))
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
 
   describe('POST/ Sign Up', () => {
     it('should return 200 on success', async () => {
       await request(app).post('/api/signup')
         .send({
-          name: 'any_name',
-          email: 'any_email@mail.com',
+          name: 'Glêsio Santos da Silva',
+          email: 'glesioss@gmail.com',
           password: 'any_password',
           passwordConfirmation: 'any_password'
         })
@@ -22,14 +29,22 @@ describe('Auth Router', () => {
     })
   })
 
-  // describe('POST/ Sign In', () => {
-  //   it('should return 200 on success', async () => {
-  //     await request(app).post('/api/signin')
-  //       .send({
-  //         email: 'any_email@mail.com',
-  //         password: 'any_password'
-  //       })
-  //       .expect(200)
-  //   })
-  // })
+  describe('POST/ Sign In', () => {
+    it('should return 200 on success', async () => {
+      const passwordHash = await bcrypt.hash('any_password', 12)
+      // inserindo um conta para teste
+      await accountCollection.insertOne({
+        name: 'Glêsio Santos da Silva',
+        email: 'glesioss@gmail.com',
+        password: passwordHash
+      })
+
+      await request(app).post('/api/signin')
+        .send({
+          email: 'glesioss@gmail.com',
+          password: 'any_password'
+        })
+        .expect(200)
+    })
+  })
 })
