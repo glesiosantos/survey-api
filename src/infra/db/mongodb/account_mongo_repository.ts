4bf122aac48800a69e19014daab720cdb78ee33d
Glêsio Santos <glesioss@gmail.com>
2023-09-29 05:06:29 -0300

    feat: add signin route

--- src/infra/db/mongodb/account_mongo_repository.ts
+++ src/infra/db/mongodb/account_mongo_repository.ts
@@ -19,7 +19,11 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
   async loadByEmail (email: string): Promise<AccountModel> {
     const accountRepository = MongoHelper.getCollection('accounts')
     const result = await accountRepository.findOne({ email })
-    // if (result) { return MongoHelper.map(result) as AccountModel }
+
+    // if (result) {
+    //   // const account = MongoHelper.map(result) as AccountModel
+    //   return MongoHelper.map(result) as AccountModel
+    // }
     return result && MongoHelper.map(result) as AccountModel
   }
 
