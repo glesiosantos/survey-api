8f206c8836e2c6524c2634da08e2ea093c769fd0
Glêsio Santos <116793448+glesiosantos@users.noreply.github.com>
2023-10-10 04:22:05 -0300

    Settings docker compose (#9)
    * chore: add engines with node version
    
    * chore: add Dockerfile
    
    * chore: add docker compose
    
    * chore: changed docker compose
    
    * chore: remove Dockerfile
    
    * chore: add dockerfile
    
    * chore: realease 1.2.0
    
    * chore: setting docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   port: process.env.PORT || 3000,
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/surveydb',
-  jwt_secret: process.env.JWT_SECRET || 'Survey2023'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/surveydb',
+  jwt_secret: process.env.JWT_SECRET || 'SurveyAPI2023'
 }
