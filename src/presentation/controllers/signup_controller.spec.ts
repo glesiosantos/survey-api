4bb6152413a238f60c23fd9416b3c11ba0cd92f3
Glêsio Santos <glesioss@gmail.com>
2024-10-13 11:14:39 -0300

    test: ensure SignUpController return 400 when e-mail is not provided

--- src/presentation/controllers/signup_controller.spec.ts
+++ src/presentation/controllers/signup_controller.spec.ts
@@ -17,18 +17,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 
-  //   it('should return 400 when email is not provided', () => {
-  //     const sut = new SignUpController()
+  it('should return 400 when email is not provided', () => {
+    const sut = new SignUpController()
 
-  //     const httpRequest = {
-  //       body: {
-  //         name: 'any_name',
-  //         password: 'any_password',
-  //         passwordConfirmation: 'any_password'
-  //       }
-  //     }
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
 
-//     const httpResponse = sut.handle(httpRequest)
-//     expect(httpResponse.statusCode).toBe(400)
-//   })
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
