f609a19012c214ea9d61db8a9e10b0eee51d745b
Glêsio Santos <glesioss@gmail.com>
2024-10-13 20:13:08 -0300

    feat: ensure SignUpController return 500 when EmailValidator throws

--- src/presentation/controllers/signup_controller.ts
+++ src/presentation/controllers/signup_controller.ts
@@ -9,13 +9,17 @@ export class SignUpController implements Controller {
   constructor (readonly emailValidator: EmailValidator) {}
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+      }
+
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) return badRequest(new InvalidParamError('email'))
+    } catch (error) {
+      return { statusCode: 500, body: new Error('Server Internal Error') }
     }
-
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValid) return badRequest(new InvalidParamError('email'))
   }
 }
